20b890d70180fe46b98bd88d011d84837bbf448c
PFalkowki <Piotr.Falkowski.fm@gmail.com>
2018-03-25 21:08:53 +0200

    Add unit tests

--- OnTheFlyStats.Test/InvariantCultureRoundingFormatTest.cs
+++ OnTheFlyStats.Test/InvariantCultureRoundingFormatTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Text;
+using Xunit;
 
 namespace OnTheFlyStats.Test
 {
@@ -17,6 +18,36 @@ namespace OnTheFlyStats.Test
             var result = tested.Format("", input, null);
 
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(-1)]
+        [InlineData(7)]
+        [InlineData(-141234)]
+        [InlineData(15)]
+        public void IntsPassThroughWhenUsingAppendFormat(int input)
+        {
+            var tested = new InvariantCultureRoundingFormat();
+
+            var stb = new StringBuilder();
+            stb.AppendFormat(tested, "{0}", input);
+            Assert.Equal(input.ToString(), stb.ToString());
+        }
+
+        [Theory]
+        [InlineData("98CD27B1-011A-436D-9E3B-3AF8F21BE88F")]
+        [InlineData("A9DA9D6B-26BC-4670-BE2E-8CEB75623600")]
+        [InlineData("3B49D899-EF77-4772-AAB5-2196FDD94F9B")]
+        [InlineData("7FA4E38F-B335-4136-BC1D-0132460F01FA")]
+        public void StringsGetThroughWhenUsingAppendFormat(string input)
+        {
+            var tested = new InvariantCultureRoundingFormat();
+
+            var stb = new StringBuilder();
+            stb.AppendFormat(tested, "{0}", input);
+            Assert.Equal(input.ToString(), stb.ToString());
         }
     }
 }
